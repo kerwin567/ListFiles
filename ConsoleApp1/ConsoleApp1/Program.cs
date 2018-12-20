b03be450ea134fa51d979c2715d9dac4c90a3f97
kerwin smith <kerwin567@hotmail.com>
2018-12-20 23:22:28 +0800

    1.追加删除文件夹内重复文件功能； 2.改写ListDirectoriesAndFiles功能，追加按条件查找文件夹及子文件夹内所有文件功能。

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,13 +13,39 @@ namespace FileSearch_Console
 
         public static void Main(string[] args)
         {
-            Console.Write("请输入要查询的目录:   ");
-            string dir = Console.ReadLine();
-            Console.Write("请输入需要导出的CSV文件地址（含文件名）：  ");
-            string CsvPath = Console.ReadLine();
             try
             {
-                ListFiles(new DirectoryInfo(dir),CsvPath);
+                Console.Write("请输入要查询的目录:   ");
+                string directory = Console.ReadLine();
+                DirectoryInfo dir = new DirectoryInfo(directory);
+                if (!dir.Exists)
+                {
+                    throw new DirectoryNotFoundException("The directory does not exist.");
+                }
+
+                
+                Console.Write("请选择功能："+"\n"+"1.导出文件夹内所有文件的文件名至csv"+"\n"+"2.删除所有文件夹内的某个文件"+"\n");
+                int chose1 = Console.ReadKey().KeyChar;
+                switch (chose1)
+                {
+                    case 49:
+                        Console.Write("\n"+"请输入需要导出的CSV文件地址（含文件名）：  ");
+                        string csvPath = directory +"\\"+ Console.ReadLine();
+                        Console.WriteLine("Enter a search string (for example *.xls): ");
+                        string search = Console.ReadLine();
+
+                        DirectoryInfo[] directoryInfos = dir.GetDirectories();
+                        FileInfo[] infos = dir.GetFiles(search);
+                        Console.WriteLine("Working...");
+                        ListDirectoriesAndFiles(directoryInfos, infos, csvPath, search);
+                        break;
+                    case 50:
+                        Console.Write("请输入文件名");
+                        string delefilename = Console.ReadLine();
+                        FileSystemInfo[] infos2 = dir.GetFileSystemInfos();
+                        Deleaa(infos2, delefilename);
+                        break;
+                }              
             }
             catch (IOException e)
             {
@@ -27,31 +53,74 @@ namespace FileSearch_Console
             }
             finally
             {
-                Console.WriteLine("done");
+                Console.WriteLine("Done");
 
             }
         }
-        public static void ListFiles(FileSystemInfo info,string csvpath)
+        public static void ListDirectoriesAndFiles(DirectoryInfo[] directoryInfos, FileInfo[] infos,string CsvPath,string SearchString="*")
         {
-            if (!info.Exists) return;
-
-            DirectoryInfo dir = info as DirectoryInfo;
             //不是目录 
-            if (dir == null) return;
-
-
-            FileSystemInfo[] files = dir.GetFileSystemInfos();
-            for (int i = 0; i < files.Length; i++)
+            if (infos == null) return;
+            if (SearchString == null || SearchString.Length == 0)
             {
-                FileInfo file = files[i] as FileInfo;
-                //是文件 
-                if (file != null)
-                    SaveCSV(csvpath, file.FullName);
-                //Console.WriteLine(file.FullName + "\t " + file.Length);
-                //对于子目录，进行递归调用 
-                else
-                    ListFiles(files[i],csvpath);
+                SearchString = "*";
             }
+            foreach (DirectoryInfo i in directoryInfos)
+            {
+                DirectoryInfo dInfo = (DirectoryInfo)i;
+
+                // Iterate through all sub-directories.
+                ListDirectoriesAndFiles(dInfo.GetDirectories(),dInfo.GetFiles(SearchString), CsvPath, SearchString);
+            }
+            foreach (FileInfo i in infos)
+            {
+                    // Save to csv.
+                    SaveCSV(CsvPath,i.FullName);
+            }
+        }
+        public static void Deleaa(FileSystemInfo[] infos,string delefilename)
+        {
+            if (infos == null) return;
+            if (delefilename == null || delefilename.Length == 0) return;
+            foreach (FileSystemInfo i in infos)
+            {
+                // Check to see if this is a DirectoryInfo object.
+                if (i is DirectoryInfo)
+                {
+                    // Cast the object to a DirectoryInfo object.
+                    DirectoryInfo dInfo = (DirectoryInfo)i;
+                    FileSystemInfo[] files = dInfo.GetFileSystemInfos();
+                    // Iterate through all sub-directories.
+                    Deleaa(files, delefilename);
+                }
+                // Check to see if this is a FileInfo object.
+                else if (i is FileInfo)
+                {
+                    // delete file
+                    i.Delete();
+                }
+            }
+            //for (int i = 0; i < files.Length; i++)
+            //{
+            //    FileInfo file = files[i] as FileInfo;
+            //    //是文件 
+            //    if (file != null)
+            //    {
+            //        if(file.Name ==delefilename)
+            //        {
+            //            file.Delete();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Deleaa(files[i], delefilename);
+            //    }
+            //SaveCSV(CsvPath, file.FullName);
+            //Console.WriteLine(file.FullName + "\t " + file.Length);
+            //对于子目录，进行递归调用 
+            //else
+            //ListFiles(files[i], CsvPath, SearchString);
+            //}
         }
         public static bool SaveCSV(string fullPath, string Data)
         {
