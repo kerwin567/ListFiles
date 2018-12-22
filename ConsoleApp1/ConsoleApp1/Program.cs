ba22f08980d13492cf69f2325d4289fd5e394cd2
kerwin smith <kerwin567@hotmail.com>
2018-12-22 21:54:11 +0800

    1.优化错误提示，console延迟关闭。 2.优化变量命名.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,48 +16,6 @@ namespace ListFileS
         {
             try
             {
-                ////调用GetProjParantPath()函数，取当前exe文件的上层文件夹
-                //string directory = GetProjParantPath();
-                ////创建文件夹对象实例 dir
-                //DirectoryInfo dir = new DirectoryInfo(directory);
-
-                ////检查文件夹是否存在
-                //if (!dir.Exists)
-                //{
-                //    throw new DirectoryNotFoundException("The directory does not exist.");
-                //}
-
-                ////创建动态csv文件，ListAllFiles_xing_当前时间.csv hh=12小时制，HH=24小时制
-                ////string csvPath = AppDomain.CurrentDomain.BaseDirectory + "\\CsvFile\\" + "ListAllFiles_xing_"+DateTime.Now.ToString("yyyyMMddHHmmss")+".csv";
-
-                ////创建静态csv文件,如果文件夹内csv文件存在，删除文件
-                //string csvPath = AppDomain.CurrentDomain.BaseDirectory + "\\CsvFile\\" + "ListAllFiles_xing.csv";
-                //FileInfo csvinfo = new FileInfo(csvPath);
-                //if (csvinfo.Exists)
-                //{
-                //    csvinfo.Delete();
-                //}
-                ////将外部参数组arges[]中的第一个参数args[0]赋值给search
-
-                //string search = "";
-                //if (args == null || args.Length < 1)
-                //{
-                //    search = "*";
-                //}
-                //else
-                //{
-                //    search = args[0];
-                //}
-
-                ////读取子目录 存到directoryInfos[]数组里
-                //DirectoryInfo[] directoryInfos = dir.GetDirectories();
-                ////返回当前目录中符合search的文件
-                //FileInfo[] infos = dir.GetFiles(search);
-
-                //Console.WriteLine("Working...");
-                ////调用主函数，搜索文件夹中的所有符合条件的文件
-                //ListDirectoriesAndFiles(directoryInfos, infos, csvPath, search);
-
                 //调用GetProjParantPath()函数，取当前exe文件的上层文件夹
                 string directory = GetProjParantPath();
                 //创建文件夹对象实例 dir
@@ -73,10 +31,10 @@ namespace ListFileS
 
                 //创建静态csv文件,如果文件夹内csv文件存在，删除文件
                 string csvPath = AppDomain.CurrentDomain.BaseDirectory + "\\CsvFile\\" + "ListAllFiles_xing.csv";
-                FileInfo csvinfo = new FileInfo(csvPath);
-                if (csvinfo.Exists)
+                FileInfo csvInfo = new FileInfo(csvPath);
+                if (csvInfo.Exists)
                 {
-                    csvinfo.Delete();
+                    csvInfo.Delete();
                 }
                 //将外部参数组arges[]中的第一个参数args[0]赋值给search
 
@@ -111,17 +69,22 @@ namespace ListFileS
             catch (IOException e)
             {
                 Console.WriteLine(e.Message);
+                while (true)
+                { }
             }
             finally
             {
                 Console.WriteLine("Done");
-
+                while (true)
+                { }
             }
         }
+
         //参数：1.文件夹中的所有子文件夹 2.当前文件夹中符合条件的文件 3.创建CSV文件的目录 4.搜索文件的条件
         //使用该函数时，需要调用：          
         //DirectoryInfo[] directoryInfos = dir.GetDirectories();//读取子目录 存到directoryInfos[]数组里
         //FileInfo[] infos = dir.GetFiles(search);//返回当前目录中符合search的文件
+
         public static void ListDirectoriesAndFiles(DirectoryInfo[] directoryInfos, FileInfo[] infos, string CsvPath, string SearchString = "*")
         {
             //不是目录 
@@ -135,10 +98,10 @@ namespace ListFileS
                 //过滤系统和隐藏文件
                 if (((i.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden) && ((i.Attributes & FileAttributes.System) != FileAttributes.System))
                 {
-                    DirectoryInfo dInfo = (DirectoryInfo)i;
+                    //DirectoryInfo dInfo = (DirectoryInfo)i;
 
                     // Iterate through all sub-directories.
-                    ListDirectoriesAndFiles(dInfo.GetDirectories(), dInfo.GetFiles(SearchString), CsvPath, SearchString);
+                    ListDirectoriesAndFiles(i.GetDirectories(), i.GetFiles(SearchString), CsvPath, SearchString);
                 }
             }
             foreach (FileInfo i in infos)
@@ -146,7 +109,7 @@ namespace ListFileS
                 //过滤系统和隐藏文件
                 if (((i.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden) && ((i.Attributes & FileAttributes.System) != FileAttributes.System))
                 {
-                    string data = i.Name + "," + i.FullName;
+                    string data = i.Name + "," + i.FullName+","+i.LastWriteTime;
                     // Save to csv.
                     SaveCSV(CsvPath, data);
                 }                
@@ -211,17 +174,18 @@ namespace ListFileS
             bool re = true;
             try
             {
-                FileStream FileStream = new FileStream(fullPath, FileMode.Append);
-                StreamWriter sw = new StreamWriter(FileStream, System.Text.Encoding.Default);
+                FileStream fileStream = new FileStream(fullPath, FileMode.Append);
+                StreamWriter sw = new StreamWriter(fileStream, System.Text.Encoding.Default);
                 sw.WriteLine(Data);
                 //清空缓冲区
                 sw.Flush();
                 //关闭流
                 sw.Close();
-                FileStream.Close();
+                fileStream.Close();
             }
-            catch
+            catch(IOException e)
             {
+                Console.WriteLine(e.Message);
                 re = false;
             }
             return re;
@@ -231,12 +195,17 @@ namespace ListFileS
         {
             //string ParantPath = "";
             //获取当前应用程序路径
-            string BaseDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
-            string TempPath = BaseDirectoryPath.Substring(0, BaseDirectoryPath.LastIndexOf("\\"));
-            string ParantPath = BaseDirectoryPath.Substring(0, TempPath.LastIndexOf("\\"));
-            return ParantPath;
-        }
+            string baseDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
 
+            ////分解路径法获得父级路径
+            //string tempPath = baseDirectoryPath.Substring(0, baseDirectoryPath.LastIndexOf("\\"));
+            //string parantPath = baseDirectoryPath.Substring(0, tempPath.LastIndexOf("\\"));
+            //return parantPath;
+            //directoryinfo类的getparant法获得父级路径
+            DirectoryInfo baseInfo = new DirectoryInfo(baseDirectoryPath);
+            DirectoryInfo parantInfo = baseInfo.Parent;
+            return parantInfo.FullName;
+        }
     }
 }
 
